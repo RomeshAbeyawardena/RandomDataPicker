204beb5dc87df771d7033ccf9aa3fae1ad1d58ed
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-05-24 22:14:39 +0100

    Added missing files

---
+++ RandomDataPicker.App/EntryPicker.cs
@@ -0,0 +1,58 @@
+﻿using RandomDataPicker.Contracts;
+using RandomDataPicker.Models;
+
+namespace RandomDataPicker.App;
+
+public sealed class EntryPicker
+{
+    private readonly IEntryProvider entryProvider;
+    private readonly IEntryPicker entryPicker;
+    private readonly IEntryInjector entryInjector;
+    private readonly Random random;
+    private IEnumerable<Entry> entries = Array.Empty<Entry>(); 
+
+    /// <summary>
+    /// Initialises the entry data and enumerates the IDs
+    /// </summary>
+    private void Initialise()
+    {
+        entries = entryProvider.GetEntries();
+        int ct = 0;
+        foreach (var entry in entries.ToArray())
+        {
+            entries = entryInjector.InjectEntry(entries, entry, random.Next(150, 150 * 3));
+            entry.Id = ct++;
+        }
+    }
+
+    public EntryPicker(IEntryProvider entryProvider, IEntryPicker entryPicker, IEntryInjector entryInjector, Random random)
+    {
+        this.entryProvider = entryProvider;
+        this.entryPicker = entryPicker;
+        this.entryInjector = entryInjector;
+        this.random = random;
+        Initialise();
+    }
+
+    public int EntryCount => entries.Count();
+    
+    /// <summary>
+    /// Injects the entry a specified number of times
+    /// </summary>
+    /// <param name="entry"></param>
+    /// <param name="numberOfTimes"></param>
+    public void Inject(Entry entry, int numberOfTimes)
+    {
+        entries = entryInjector.InjectEntry(entries, entry, numberOfTimes);
+    }
+
+    /// <summary>
+    /// Gets entries from source
+    /// </summary>
+    /// <param name="numberOfEntries"></param>
+    /// <returns></returns>
+    public IEnumerable<Entry> GetEntries(int numberOfEntries)
+    {
+        return entryPicker.PickEntries(entries, numberOfEntries);
+    }
+}
---
+++ RandomDataPicker.App/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using RandomDataPicker.App;
+using RandomDataPicker.Core;
+
+var services = new ServiceCollection()
+    .RegisterServices("entries.json")
+    .AddSingleton<EntryPicker>()
+    .BuildServiceProvider();
+
+var appEntryPicker = services.GetRequiredService<EntryPicker>();
+
+appEntryPicker.Inject(new RandomDataPicker.Models.Entry {
+    City = "Lincoln",
+    Email = "romesh.a@outlook.com",
+    IsFlagged = false,
+    Name = "Romesh Abeyawardena"
+}, 150 * 20);
+
+var userEntry = new RandomDataPicker.Models.Entry { IsFlagged = true };
+
+Console.Write("Name: ");
+userEntry.Name = Console.ReadLine();
+Console.Write("Email: ");
+userEntry.Email = Console.ReadLine();
+Console.Write("City: ");
+userEntry.City = Console.ReadLine();
+Console.Write("Tickets (default:3): ");
+if (!int.TryParse(Console.ReadLine(), out var tickets))
+{
+    tickets = 3;
+}
+
+appEntryPicker.Inject(userEntry, 150 * tickets);
+
+
+using var registration = new CancellationTokenSource();
+
+var separater = new string('#', Console.WindowWidth);
+var lineSpace = new string(' ', Console.WindowWidth / 2 - 24);
+var lineSeparater = new string('-', Console.WindowWidth);
+while (!registration.Token.IsCancellationRequested)
+{
+    Console.WriteLine($"🎫 Out of {appEntryPicker.EntryCount} entries, the winners are... 🏅");
+    foreach (var entry in appEntryPicker.GetEntries(5))
+    {
+        if (entry.IsFlagged)
+        {
+            Console.WriteLine(separater);   
+            Console.WriteLine($"{lineSpace}Congratulations you won!{lineSpace}");
+            Console.WriteLine(separater);
+        }
+        Console.WriteLine(entry);
+    }
+    var choice = Console.ReadKey(true);
+
+    if(choice.Key != ConsoleKey.Y || choice.Key == ConsoleKey.Escape)
+    {
+        registration.Cancel();
+    }
+    
+    Console.WriteLine(lineSeparater);
+}
