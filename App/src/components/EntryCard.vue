31aba6184406187c184097cb8d080ef0404c1ca5
romeshabeyawardena <RomeshAbeyawardena@users.noreply.github.com>
2023-05-28 22:50:10 +0100

    Updated icons

--- App/src/components/EntryCard.vue
+++ App/src/components/EntryCard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+    import "../scss/entry-card.scss";
     import { Entry } from "../models/entry";
     import { PersistedEntry } from "../models/persistedEntry";
     import { DateHelper } from "../helpers/dateHelpers";
@@ -14,13 +15,13 @@
     }
 </script>
 <template>
-    <div v-if="props.entryCard != undefined">
+    <div class="entry-card" v-if="props.entryCard != undefined">
         <h2 v-if="props.entryCard?.isFlagged">🏅 You Won! 🏅</h2>
         <h3>{{ props.entryCard?.name }}</h3>
         <p v-if="props.showEmail">{{ props.entryCard?.email }}</p>
         <p class="mb-2">{{ props.entryCard?.city }}</p>
     </div>
-    <div v-if="props.persistedEntry != undefined">
+    <div class="entry-card" v-if="props.persistedEntry != undefined">
         <h2 v-if="props.persistedEntry?.isFlagged">🏅 You Won! 🏅</h2>
         <h3>{{ props.persistedEntry?.name }}</h3>
         <p v-if="props.showEmail">{{ props.entryCard?.email }}</p>
